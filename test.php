a26d69b6fde40f8c72438773516f9686651f0854
Daniel <danielleewaite@hotmail.co.uk>
2016-01-29 13:26:31 +0000

    adding test file

---
+++ test.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+
+require_once("php-foursquare/src/FoursquareApi.php");
+
+$foursquare = new FoursquareApi("OZ2IWKQWSXNOA5IUR2ZOBNL3O340CIFZ0DYBQFOG54CUAL0Q", "VRJAMLKNAWZKT5SVJ0TCR0SRQ4DDKCOGSAPE4BUKICXUGKW1");
+
+// Searching for venues nearby Montreal, Quebec
+$endpoint = "venues/search";
+
+// Prepare parameters
+$params = array("near"=>"Montreal, Quebec");
+
+// Perform a request to a public resource
+$response = $foursquare->GetPublic($endpoint,$params);
+
+// Returns a list of Venues
+// $POST defaults to false
+$venues = $foursquare->GetPublic($endpoint [,$params], $POST=false);
+print_r($venues);
+?>
+</body>
+</html>
