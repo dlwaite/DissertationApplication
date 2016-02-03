8bae4186bc1ada350a89fc2f981f2ebf5031b3ef
Daniel <danielleewaite@hotmail.co.uk>
2016-02-03 12:37:17 +0000

    edited code, the table did not work

--- searchResults.php
+++ searchResults.php
@@ -24,43 +24,24 @@ $params = array("near"=>$searchPlace);
 $response = $foursquare->GetPublic($endpoint,$params);
 
 // Returns a list of Venues
-// $POST defaults to false
 $venues = $foursquare->GetPublic($endpoint ,$params, $POST=false);
 
-$queryresult = mysql_query($venues) or die (mysql_error());
+//$queryresult = mysql_query($venues) or die (mysql_error());
 
-while ($row = mysql_fetch_assoc($queryresult)) {
-	$id = $row['id'];
-	$name = $row['name'];
-	//$three = $row['contact'];
-	//$four = $row['location'];
-	//$five = $row['address'];
-	//$six = $row['lat'];
-	//$seven = $row['lng'];
-	//$eight = $row['postalCode'];
-	//$nine = $row['cc'];
-	//$ten = $row['city'];
-	//$eleven = $row['country'];
-	//$twelve = $row['formattedAddress'];
+//while ($row = mysql_fetch_assoc($queryresult)) {
+//	$id = $row['id'];
+//	$name = $row['name'];
 	
-	echo "<div class=\"Venues\">";
-	echo "<span class=\"id\">$id. </span>";
-	echo "<span class=\"name\">$name. </span>";
-	//echo "<span class=\"contact\">$three. </span>";
-	//echo "<span class=\"location\">$four. </span>";
-	//echo "<span class=\"address\">$five. </span>";
-	//echo "<span class=\"lat\">$six. </span>";
-	//echo "<span class=\"lng\">$seven. </span>";
-	//echo "<span class=\"postalCode\">$eight. </span>";
-	//echo "<span class=\"cc\">$nine. </span>";
-	//echo "<span class=\"city\">$ten. </span>";
-	//echo "<span class=\"country\">$eleven. </span>";
-	//echo "<span class=\"formattedAddress\">$twelve. </span>";
- 	echo "</div>";
+//	echo "<div class=\"Venues\">";
+//	echo "<span class=\"id\">$id. </span>";
+//	echo "<span class=\"name\">$name. </span>";
+// 	echo "</div>";
 	
-}
+//}
 
-mysql_free_result($queryresult);
+//mysql_free_result($queryresult);
+
+print_r($venues)
 
 ?>
 <br>
