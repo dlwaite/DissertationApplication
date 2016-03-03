8cd30dbce6395207b8b4db13a2f44f1daa97cb54
Daniel <danielleewaite@hotmail.co.uk>
2016-03-03 15:21:29 +0000

    attempting to add date

--- searchResults.php
+++ searchResults.php
@@ -3,17 +3,32 @@
 <head>
 <meta charset="utf-8">
 <title>Search Results</title>
+<link href="stylesheet.css" rel="stylesheet" type="text/css" media="all" id="styleSheet" />
 </head>
 
 <body>
 <form id="SearchResults" action="" method="post">
 <?php
+//include 'datasbase_conn.php';	  // make database connection
+include 'functions.php';	  // make database connection
+
 $searchPlace = $_GET['searchPlace'];
 $activity = $_GET['category'];
+$date = $_GET['date'];
 
 require_once("FoursquareApi.php");
+require_once("https://www.github.com/Ents24/ents24-api-client.git");
 
 $foursquare = new FoursquareApi("OZ2IWKQWSXNOA5IUR2ZOBNL3O340CIFZ0DYBQFOG54CUAL0Q", "VRJAMLKNAWZKT5SVJ0TCR0SRQ4DDKCOGSAPE4BUKICXUGKW1");
+
+$client = Ents24\Api\Client::factory(
+    [
+        'client_id'     => 'e5cb720f71f55be8e448f3a5956ac8b9d0249c9d',
+        'client_secret' => '06a69663a2f99b1080ffeea4c942e7a82fd0c10c;',
+    ]
+);
+
+
 
 // Searching for venues nearby Montreal, Quebec
 $endpoint = "venues/search";
