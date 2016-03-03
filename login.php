1e05f550ca631b3c3f7387d1d28ce5802623fc2a
Daniel <danielleewaite@hotmail.co.uk>
2016-03-03 13:33:27 +0000

    updating connection link

--- login.php
+++ login.php
@@ -20,13 +20,6 @@
             <div><p class="action"><input type="submit" name="submit" value="Log In" /></p></div>
             <p><a href="index.php">Back to Homepage</a></p>
 	</form>
-    
-	<!-- add the w3 validator to the bottom right of the page -->
-	<div align="right"><p>
-		<a href="http://validator.w3.org/check?uri=referer"><img
-    	src="http://www.w3.org/Icons/valid-xhtml10"
-    	alt="Valid XHTML 1.0!" height="31" width="88" /></a>
-	</p></div>
 
 </body>
 </html>
