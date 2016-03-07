b48f79325190be26c8c439b4be6c7c822167af05
Daniel <danielleewaite@hotmail.co.uk>
2016-03-07 14:46:51 +0000

    working on log in

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ function loginStatus () {
 	// if the user was logged in and there was a logged in session in the session
 	if (isset($_SESSION['logged-in']) && ($_SESSION['logged-in'])) {
 		// create a variable to store the username of the user who is logged in
-		$username = $_SESSION['username'];
+		$firstName = $_SESSION['firstName'];
 		?>
 		<div align="right"> <?php 		// after aligning it to the right, say hello user and then include the username of the user logged in, and next to it have a log out button
-			echo "<p>Hello user: $username"; echo "<input type=\"submit\" value=\"Log Out\" action=\"logout.php\"/></p>"; ?>
+			echo "<p>Hello: $firstName"; echo "<input type=\"submit\" value=\"Log Out\" action=\"logout.php\"/></p>"; ?>
 		</div>
 		
 		<?php
@@ -40,19 +40,26 @@ function logIn () {
 		 die (mysql_error());  
 	}
 	else {																	// else if the fields are not left empty
-		$sql = "SELECT password FROM tbl_users WHERE emailAddress = " & $username & "";		// select the password and salt from the users table where the username matches the username entered
-
-		$stmt = mysqli_prepare($conn, $sql); 
+		$sql = "SELECT password, firstName FROM tbl_users WHERE emailAddress = " & $username & "";		// select the password and salt from the users table where the username matches the username entered
 		
-		mysqli_stmt_bind_param($stmt, "s", $username);
-		mysqli_stmt_execute($stmt); 
+		$queryresult = mysql_query($sql) or die (mysql_error());
 		
-		mysqli_stmt_bind_result($stmt, $passwordfromDB);
+		if (mysql_num_rows($queryresult) > 0) {
+			$userDetailsrow = mysql_fetch_array($queryresult);
+			$firstName = $userDetailsrow['firstName'];
+			$passwordFromDB = $userDetailsrow['password'];
 
-		if (mysqli_stmt_fetch($stmt)) {										// if there was a result
+		//$stmt = mysqli_prepare($conn, $sql); 
+		
+		//mysqli_stmt_bind_param($stmt, "s", $username);
+		//mysqli_stmt_execute($stmt); 
+		
+		//mysqli_stmt_bind_result($stmt, $passwordfromDB);
+
+		//if (mysqli_stmt_fetch($stmt)) {										// if there was a result
 	
 			if ($password == $passwordfromDB) {								// if the passwords match
-				$_SESSION['username'] = $username;							// store the value of the user logged in in the session
+				$_SESSION['firstName'] = $firstName;						// store the value of the user logged in in the session
 				$_SESSION['logged-in'] = true;								// make the session logged in as true
 				echo "<p>You have successfully signed in.</p>";				// tell the user they have logged in and include a link to the admin page
 				echo "<a href = \"admin.php?username=$username\">Continue to Admin Page</a>";
