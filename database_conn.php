676fee83faf84074db0392ab632511346eee36fb
Daniel <danielleewaite@hotmail.co.uk>
2016-03-07 15:49:09 +0000

    working on log in

--- database_conn.php
+++ database_conn.php
@@ -5,14 +5,17 @@ define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 
-$dbhost = constant("DB_HOST"); // Host name 
-$dbport = constant("DB_PORT"); // Host port
-$dbusername = constant("DB_USER"); // Mysql username 
-$dbpassword = constant("DB_PASS"); // Mysql password 
-$db_name = constant("DB_NAME"); // Database name 
+//$dbhost = constant("DB_HOST"); // Host name 
+//$dbport = constant("DB_PORT"); // Host port
+//$dbusername = constant("DB_USER"); // Mysql username 
+//$dbpassword = constant("DB_PASS"); // Mysql password 
+//$db_name = constant("DB_NAME"); // Database name 
 
-$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
-mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
+$mysqlCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, "", DB_PORT) or die("Error: " . mysqli_error($mysqlCon));
+mysqli_select_db($mysqlCon, DB_NAME) or die("Error: " . mysqli_error($mysqlCon));
+
+//$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
+//mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 
 
 //Root User: adminQTSXp5f
