3e42f4461c1c96ea513a5f954a27e610ef58c1b1
Daniel <danielleewaite@hotmail.co.uk>
2016-03-07 15:26:51 +0000

    working on log in

--- database_conn.php
+++ database_conn.php
@@ -1,27 +1,18 @@
 <?php //connect to the database
-//define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-//define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-//define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-//define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-//define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
 
-//$dbhost = constant("DB_HOST"); // Host name 
-//$dbport = constant("DB_PORT"); // Host port
-//$dbusername = constant("DB_USER"); // Mysql username 
-//$dbpassword = constant("DB_PASS"); // Mysql password 
-//$db_name = constant("DB_NAME"); // Database name 
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME"); // Database name 
 
-//$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
-//mysqli_select_db($m ysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
-
-// procedural style
-$mysqli = mysqli_connect(
-    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
-    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
-    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
-    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
-    getenv('OPENSHIFT_MYSQL_DB_PORT')
-);
+$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
+mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 
 
 //Root User: adminQTSXp5f
@@ -34,11 +25,5 @@ $mysqli = mysqli_connect(
 //  Root User: adminQTSXp5f
 //  Root Password: agISJEaCtxUB
 //URL: https://phpmyapp-dlwaite.rhcloud.com/phpmyadmin/
-
-//$conn = mysql_connect ('mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/', 'adminQTSXp5f', 'agISJEaCtxUB')
-//	or die("Could Not Connect to MySQL!");
-//	mysql_select_db("phpmyapp")
-//	or die("Could Not Open Database:".mysql_error());
-
 
 ?>
