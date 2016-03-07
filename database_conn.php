1c21b925f5b9259f2711a39d18c7ccdf6232ecdb
Daniel <danielleewaite@hotmail.co.uk>
2016-03-07 14:04:28 +0000

    fixing database connection

--- database_conn.php
+++ database_conn.php
@@ -1,8 +1,24 @@
 <?php //connect to the database
-$conn = mysql_connect ('mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/', 'adminQTSXp5f', 'agISJEaCtxUB')
-	or die("Could Not Connect to MySQL!");
-	mysql_select_db("phpmyapp")
-	or die("Could Not Open Database:".mysql_error());
+//$conn = mysql_connect ('mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/', 'adminQTSXp5f', 'agISJEaCtxUB')
+//	or die("Could Not Connect to MySQL!");
+//	mysql_select_db("phpmyapp")
+//	or die("Could Not Open Database:".mysql_error());
+
+
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+$dbpassword = constant("DB_PASS"); // Mysql password 
+$db_name = constant("DB_NAME"); // Database name 
+
+$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
+mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 
 
 //Root User: adminQTSXp5f
