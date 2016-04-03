edb5377fe164eed828909442567c4721b6700141
Daniel <danielleewaite@hotmail.co.uk>
2016-04-03 23:54:25 +0100

    working on google map api

--- itinerary.php
+++ itinerary.php
@@ -103,13 +103,6 @@ print $cart
 
 <h1>Please check your Itinerary...</h1>
 
-<?php
-echo itineraryContents();
-
-?>
-
-<br><br>
-
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script> 
 <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> 
 <script type="text/javascript"> 
@@ -121,37 +114,75 @@ echo itineraryContents();
             zoom: 14,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
-        var google_map = new google.maps.Map(document.getElementById("map_canvas"), map_options);
+		
+		var google_map = new google.maps.Map(document.getElementById("map_canvas"), map_options);
 
         var info_window = new google.maps.InfoWindow({
             content: 'loading'
         });
 
-        var t = [];
+<?php
+$cart = $_SESSION['cart'];
+	if ($cart) {
+		$items = explode(',',$cart);
+	
+		// create a table
+		echo "<table border='1'>";
+		// with headings
+		echo "<tr><th>Name</th><th>Category</th><th>Latitude</th><th>Longitude</th></tr>";
+		?>
+		var t = [];
         var x = [];
         var y = [];
         var h = [];
-		
 		<?php
-		$lat = $_SESSION['lat'];
-		if ($lat) {
-		$latitude = explode(',',$lat);
-		
-		foreach ($latitude as $id) {
+		foreach ($items as $id) {
 			
-		?>
+			require_once("FoursquareApi.php");
 
-        t.push('Location Name 1');
-        x.push(<?php $lat ?>);
-        y.push(-84.35686);
-        h.push('<p><strong>Location Name 1</strong><br/>Address 1</p>');
+			$foursquare = new FoursquareApi("OZ2IWKQWSXNOA5IUR2ZOBNL3O340CIFZ0DYBQFOG54CUAL0Q", "VRJAMLKNAWZKT5SVJ0TCR0SRQ4DDKCOGSAPE4BUKICXUGKW1");
+			
+			$endpoint = "venues/$id";
+			
+			// Prepare parameters
+			$params = array();
+			
+			// Perform a request to a public resource
+			$response = $foursquare->GetPublic($endpoint,$params);
+			
+			$venues = json_decode($response);
+			
+			echo "<tr><td>";
+			echo $venues->response->venue->name.'<br>';
+			echo "</td><td>";
+
+			?>
+        	t.push(<?php $venues->response->venue->name ?>);
+			x.push(<?php $venues->response->venue->location->lat ?>);
+			y.push(<?php $venues->response->venue->location->lng ?>);
+			h.push(<?php $venues->response->venue->name ?>);
+
+			<?php
+			if(isset($venues->response->venue->categories['0']))
+                {
+					if(property_exists($venues->response->venue->categories['0'],"name"))
+					{ 
+					echo $venues->response->venue->categories['0']->name.'<br>';
+					echo "</td><td>";
+					}
+				}
 		
-		<?php
+			echo "".$venues->response->venue->location->lat."";
+			echo "</td><td>";
+			echo "".$venues->response->venue->location->lng."";
+			echo "</td></tr>";			  
 		}
-		}
-		?>
+		echo "</table>";
+	} else {
+		echo '<p>Your itinerary is empty.</p>';
+}
 
+?>
         var i = 0;
         for ( item in t ) {
             var m = new google.maps.Marker({
@@ -175,11 +206,6 @@ echo itineraryContents();
 
 
 <p>|| <a href="./index.php">Add Further Venues</a> ||
-
-
-<?php 
-print $lat;
-?>
 
 </body>
 </html>
