4d793850f042027bde48e463026837d12425e391
Daniel <danielleewaite@hotmail.co.uk>
2016-04-05 17:16:09 +0100

    working on activity dropdown

--- searchResults.php
+++ searchResults.php
@@ -35,8 +35,29 @@ $date = $_GET['date'];
 <label for="category">Select Activity</label> 
 <select name="category" >
    <option value="">Please Select</option>
+   <optgroup label="Entertainment">
+   <option value="56aa371be4b08b9a8d5734db">Amphitheater</option>
+   <option value="4fceea171983d5d06c3e9823">Aquarium</option>
+   <option value="4bf58dd8d48988d1e1931735">Arcade</option>
+   <option value="4bf58dd8d48988d1e2931735">Art Gallery</option>
+   </optgroup>
+   <option value="4bf58dd8d48988d116941735">Bar</option>
    <option value="4bf58dd8d48988d1e4931735">Bowling Alley</option>
-   <option value="4bf58dd8d48988d18e941735">Comedy Club</option>
+   <option value="4bf58dd8d48988d17c941735">Casino</option>
+   <option value="4bf58dd8d48988d18e941735">Comedy Club</option>   
+   <option value="5032792091d4c4b30a586d5c">Concert Hall</option>
+   <option value="4d4b7105d754a06374d81259">Food Venue</option>
+   <option value="52e81612bcbc57f1066b79ea">Go Kart Track</option>
+   <option value="52e81612bcbc57f1066b79eb">Mini Golf</option>
+   <option value="4bf58dd8d48988d17f941735">Movie Theater</option>
+   <option value="4bf58dd8d48988d181941735">Museum</option>
+   <option value="4bf58dd8d48988d1e5931735">Music Venue</option>
+   <option value="4bf58dd8d48988d1e3931735">Pool Hall</option>
+   <option value="56aa371be4b08b9a8d573514">Racecourse</option>
+   <option value="4bf58dd8d48988d184941735">Stadium</option>  
+   <option value="4bf58dd8d48988d182941735">Theme Park</option>
+   <option value="4bf58dd8d48988d193941735">Water Park</option>
+   <option value="4bf58dd8d48988d17b941735">Zoo</option>
 </select>
 &nbsp;&nbsp;
 <label for="date">Select Date</label>
