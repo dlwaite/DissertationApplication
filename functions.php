a440bb6b7eb3f7f200f1afcb97bd958629d87b51
Daniel <danielleewaite@hotmail.co.uk>
2016-04-06 00:38:21 +0100

    working on remove all button into itinerary page

--- functions.php
+++ functions.php
@@ -132,10 +132,10 @@ function itineraryContents() {
 			echo "".$venues->response->venue->location->lng."";
 			echo "</td><td>";
 			echo "<a href=\"itinerary.php?action=delete&id=".$venues->response->venue->id."\">Remove</a>";
-		    echo "</td></tr>";			  
-		}
-		echo "<a href=\"itinerary.php?action=removeAll\">Clear Itinerary</a>";
-		echo "</td></tr>";	
+		    echo "</td></tr>";	
+			echo "<a href=\"itinerary.php?action=removeAll\">Clear Itinerary</a>";
+			echo "</td></tr>";		  
+		}	
 		echo "</table>";
 	} else {
 		echo '<p>Your itinerary is empty.</p>';
