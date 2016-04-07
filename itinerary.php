38b2896ff2752db56fc56815dcba7387db2f54e9
Daniel <danielleewaite@hotmail.co.uk>
2016-04-08 00:14:41 +0100

    commenting code

--- itinerary.php
+++ itinerary.php
@@ -105,7 +105,7 @@ echo itineraryStatus();
 <p><h2>Search for Further Venues</h2></p>
 <!-- create the text box for the search location -->
 <label for="searchPlace">Please Enter the Search Location</label>
-<input type="text" name="searchPlace" id="searchPlace" maxlength="254" size="30" value="<?php $searchPlace ?>" />
+<input type="text" name="searchPlace" id="searchPlace" maxlength="254" size="30" />
 &nbsp;&nbsp;
 <!-- create the activity dropdown list -->
 <label for="category">Select Activity</label> 
